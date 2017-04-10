cbbdb24f17bf6fda171b415dff3e6d5f5a3774f9
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-04-10 16:03:50 -0400

    Created simple node server

---
+++ image-server-api/app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var cors = require('cors');
+
+var index = require('./routes/index');
+
+var app = express();
+app.use(cors());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Connect to MongoDB here
+//EX: mongoose.connect('mongodb://52.14.32.168');
+port = 3000;
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// Require the needed routes for each project component
+// EX: require('./routes/user/users.js')(app);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+app.listen(port, function(){
+  console.log("Express server listening on port 3000");
+});
+
+module.exports = app;
