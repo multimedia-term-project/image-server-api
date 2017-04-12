314ab6c23ada0e8a40e579f5ad68b758e027b2e6
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-04-12 15:14:36 -0400

    Added Multer Package to recieve pictures

--- image-server-api/routes/image/image.js
+++ image-server-api/routes/image/image.js
@@ -1,20 +1,27 @@
 var aws = require('aws-sdk');
+var shortid = require('shortid');
+var multer  = require('multer')
+
 aws.config.loadFromPath('./config.json');
 var s3 = new aws.S3();
 
 module.exports = function(app) {
-  app.post("/image/:userId", function(req, res){
+  var upload = multer();
+  app.post("/image/:userId", multer().any(), function(req, res){
+    var fileName = shortid.generate() + "-" + req.files[0].originalname;
     var params = {
       Bucket : "multimedia-term-project",
       ACL : "public-read",
-      Key : req.body.name,
-      Body : req.file
+      Key : fileName,
+      Body : req.files[0].buffer
     };
     s3.putObject(params, function(err, data) {
       if (err) {
         res.status(500).send(err)
       }
-      res.json(data);
+      res.json({
+          url : "https://s3.us-east-2.amazonaws.com/multimedia-term-project/" + fileName
+      });
     });
   });
 }
